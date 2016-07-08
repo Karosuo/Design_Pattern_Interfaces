26575a019d48544061b8d3cd38756544db69c426
Rafael_Karosuo <rafaelkarosuo@gmail.com>
2016-07-07 20:46:37 -0700

    OneWin Drill down complete dir

---
+++ OneWinDrillDown/www/js/OneWinDrillDown.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded',function(){
+   var folders = ["Acciones", "Comida", "Juguetes", "Lugares", "Pronombres","Sentimientos"];
+    myPhraseContainer = new PhraseRow("PhraseContainer");           
+    
+    var myImagePanel = new ImageGridPanel(6,"PatternContainer",myPhraseContainer);
+    myImagePanel.setFolder(folders[0]); 
+    myImagePanel.loadPics();    
+    myImagePanel.loadSelector(15);
+    
+    myUtils = new myUtilities("PatternContainer",myImagePanel,folders);     
+    myUtils.setupDrillDown();
+
+    //myDrawerRM = new Drawer_RM("PatternContainer", folders, "Drawer");
+});
+
+$(window).load(function(){
+        //calls the myUtilities object and puts the floated divs
+    myUtils.loadFloatDivs();
+        //adds listener to the orientation change, reposition the floated divs
+    window.addEventListener('orientationchange', doOnOrientationChange);
+});
+
+/*Not an object
+Detects the chang of orientation*/
+function doOnOrientationChange()
+{
+    myUtils.loadFloatDivs();
+}
